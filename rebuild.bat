7d095833fda21b09968f4c4763636ad5ec74f9ee
ymj68520 <ymj68520@std.uestc.edu.cn>
2026-03-05 11:52:31 +0800

    Remove Visual Studio Project files

--- rebuild.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" ForensicsProject.vcxproj /p:Configuration=Release /p:Platform=x64 /t:Rebuild /m /nologo
-if errorlevel 1 (
-    echo Build failed!
-    exit /b 1
-)
-echo Build succeeded!
